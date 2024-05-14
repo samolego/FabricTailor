e327d9dc9f186b22f49462cc3eb74906961210bd
samolego <34912839+samolego@users.noreply.github.com>
2024-05-14 11:18:38 +0200

    Fixes #101 Fixes #100

--- src/main/java/org/samo_lego/fabrictailor/mixin/client/MSkinCustomizationScreen_SkinButton.java
+++ src/main/java/org/samo_lego/fabrictailor/mixin/client/MSkinCustomizationScreen_SkinButton.java
@@ -1,30 +1,44 @@
 package org.samo_lego.fabrictailor.mixin.client;
 
 import net.minecraft.client.Options;
+import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.OptionsList;
 import net.minecraft.client.gui.screens.OptionsSubScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.SkinCustomizationScreen;
+import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import org.jetbrains.annotations.Nullable;
 import org.samo_lego.fabrictailor.client.ClientTailor;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.util.List;
+
 
 @Mixin(SkinCustomizationScreen.class)
 public class MSkinCustomizationScreen_SkinButton extends OptionsSubScreen {
+
     public MSkinCustomizationScreen_SkinButton(Screen screen, Options options, Component component) {
         super(screen, options, component);
     }
 
-    @Inject(method = "init", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
-    private void onInit(CallbackInfo ci, int i) {
-        if (this.minecraft.player != null) {
-            this.addRenderableWidget(Button.builder(Component.literal("FabricTailor"),
-                    button -> this.minecraft.setScreen(ClientTailor.SKIN_CHANGE_SCREEN)).bounds(this.width / 2 - 100, this.height / 6 + 24 * (i >> 1) + 22, 200, 20).build());
+    @Inject(method = "init",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/components/OptionsList;addSmall(Ljava/util/List;)V"
+            ),
+            locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void onInit(CallbackInfo ci, List<AbstractWidget> widgets) {
+        if (this.minecraft != null && this.minecraft.player != null) {
+            var ftButton = Button.builder(Component.literal("FabricTailor"),
+                    button -> this.minecraft.setScreen(ClientTailor.SKIN_CHANGE_SCREEN)).build();
+            widgets.add(ftButton);
         }
     }
 }
