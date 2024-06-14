433897314483d7164805c72f739b70206e8d3d33
devourmealive <foxmanlisichka@yandex.ru>
2024-06-14 10:40:46 +0300

    FABRICTAILOR-107 fixed NPE on invalid url

--- src/main/java/org/samo_lego/fabrictailor/mixin/client/MTextureUrlChecker_AllDomains.java
+++ src/main/java/org/samo_lego/fabrictailor/mixin/client/MTextureUrlChecker_AllDomains.java
@@ -2,25 +2,33 @@ package org.samo_lego.fabrictailor.mixin.client;
 
 import com.google.common.net.InternetDomainName;
 import com.mojang.authlib.yggdrasil.TextureUrlChecker;
+import java.net.URI;
+import static org.samo_lego.fabrictailor.FabricTailor.config;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import java.net.URI;
-
-import static org.samo_lego.fabrictailor.FabricTailor.config;
-
 @Mixin(value = TextureUrlChecker.class, remap = false)
-public class MTextureUrlChecker_AllDomains {
+public final class MTextureUrlChecker_AllDomains {
+
+    private static final String FILE_SCHEME_CONST = "file";
+
+    private MTextureUrlChecker_AllDomains() {
+    }
 
     @Inject(method = "isAllowedTextureDomain",
             at = @At(value = "INVOKE", target = "Ljava/net/URI;getScheme()Ljava/lang/String;"),
-            locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+            locals = LocalCapture.CAPTURE_FAILHARD,
+            cancellable = true)
     private static void ft_allowAllTextureDomains(String url, CallbackInfoReturnable<Boolean> cir, URI uri) {
-        if (uri.getScheme().equals("file")) {
+        if (FILE_SCHEME_CONST.equals(uri.getScheme())) {
             cir.setReturnValue(false);  // todo, allow files
+            return;
+        }
+
+        if (uri.getHost() == null) {
             return;
         }
 
@@ -32,4 +40,5 @@ public class MTextureUrlChecker_AllDomains {
         var allowed = config.allowedTextureDomains.contains(topDomain);
         cir.setReturnValue(allowed);
     }
+
 }
