b37c2ed3dd8945c8656fb2c6a0810986bd5f4ff6
DizzyNight <laffey@dizzynight.moe>
2025-10-18 21:15:30 +0800

    fix: update override mouseClicked method signature

--- src/main/java/org/samo_lego/fabrictailor/client/screen/SkinChangeScreen.java
+++ src/main/java/org/samo_lego/fabrictailor/client/screen/SkinChangeScreen.java
@@ -17,6 +17,7 @@ import net.minecraft.client.gui.screens.advancements.AdvancementsScreen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.client.gui.screens.inventory.tooltip.ClientTooltipComponent;
 import net.minecraft.client.gui.screens.inventory.tooltip.DefaultTooltipPositioner;
+import net.minecraft.client.input.MouseButtonEvent;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
@@ -359,18 +360,17 @@ public class SkinChangeScreen extends Screen {
      * Checks if one of the tabs was clicked
      * and selects it accordingly.
      *
-     * @param mouseX mouse x
-     * @param mouseY mouse y
-     * @param button button that was clicke
+     * @param mouseButtonEvent The mouse event
+     *
      * @return super.mouseClicked()
      */
     @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        if (button == 0) {
+    public boolean mouseClicked(MouseButtonEvent mouseButtonEvent, boolean bl) {
+        if (mouseButtonEvent.button() == 0) {
             for (int i = 0; i < TABS.size(); ++i) {
                 SkinTabType tab = TABS.get(i);
 
-                final boolean mouseOver = tab.getTabType().isMouseOver(startX, startY, tab.getTabType().getMax() - i - 1, mouseX, mouseY);
+                final boolean mouseOver = tab.getTabType().isMouseOver(startX, startY, tab.getTabType().getMax() - i - 1, mouseButtonEvent.x(), mouseButtonEvent.y());
 
                 if (mouseOver) {
                     this.selectedTab = tab;
@@ -378,7 +378,7 @@ public class SkinChangeScreen extends Screen {
                 }
             }
         }
-        return super.mouseClicked(mouseX, mouseY, button);
+        return super.mouseClicked(mouseButtonEvent, bl);
     }
 
 
